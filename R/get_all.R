4e8a2b0706c2550de8784a720ceb68c8bc6303db
fawda123 <mbafs2012@gmail.com>
2017-11-12 12:44:37 -0600

    added get_fin and get_all functions

---
+++ R/get_all.R
@@ -0,0 +1,43 @@
+#' Get combined conditional probabilities
+#'
+#' Get combined conditional probabilities
+#' 
+#' @param restdat \code{data.frame} of restoration data
+#' @param reststat \code{data.frame} of restoration data locations 
+#' @param wqdat \code{data.frame} of water quality data
+#' @param wqstat \code{data.frame} of water quality data locations 
+#' @param mtch numeric value of number of restoration sites to match to each water quality site
+#' @param yrdf numeric value of year window for evaluation water quality differences before/after each restoration project
+#' @param resgrp chr string for grouping variable of restoration project types
+#' @param qts numeric vector of quantile values for conditional probabilities
+#' @param lbs labels for defining quantile values in \code{qts}
+#' @param ... additional arguments as chr strings for grouping variables
+#'
+#' @return
+#' @export
+#'
+#' @examples
+get_all <- function(restdat, reststat, wqdat, wqstat, mtch = 10, yrdf = 5, resgrp = 'top', qts = c(0.33, 0.66), 
+                    lbs = c('lo', 'md', 'hi'), ...){
+ 
+  ## Distance to restoration sites
+  wqmtch <- get_clo(restdat, reststat, wqstat, resgrp = resgrp, mtch = mtch)
+  
+  ## Summarizing effects of restoration projects on salinity
+  salchg <- get_chg(wqdat, wqmtch, statdat, restdat, wqvar = 'sal', yrdf = yrdf)
+  
+  ## Summarizing effects of restoration projects on chlorophyll
+  chlchg <- get_chg(wqdat, wqmtch, statdat, restdat, wqvar = 'chla', yrdf = yrdf)
+  
+  # Get conditional probability distributions for the restoration type on salinity 
+  wqcdt <- get_cdt(salchg, ...)
+  
+  # Discretization of salinity conditional probability distributions: 
+  salbrk <- get_brk(wqcdt, qts = qts, ...)
+  
+  # get final conditional probabiliyt for last child node:
+  allchg <- get_fin(chlchg, salbrk, salchg, lbs = lbs, ...)
+  
+  return(allchg)
+   
+}
---
+++ R/get_fin.R
@@ -0,0 +1,46 @@
+#' Get final conditional probability for last child node
+#'
+#' @param chlchg 
+#' @param salbrk 
+#' @param salchg
+#' @param lbs
+#' @param ... additional arguments as chr strings for grouping variables
+#'
+#' @return
+#' @export
+#'
+#' @examples
+get_fin <- function(chlchg, salbrk, salchg, lbs = c('lo', 'md', 'hi'), ...){
+  
+  # merge with salinity, bet salinity levels
+  salbrk <- salbrk %>% 
+    group_by_(...) %>% 
+    nest(.key = 'levs')
+  allchg <- full_join(chlchg, salchg, by = c('hab', 'wtr', 'stat')) %>% 
+    rename(
+      salev = cval.y, 
+      cval = cval.x
+    ) %>% 
+    group_by_(...) %>% 
+    nest %>% 
+    left_join(salbrk, by = c('hab', 'wtr')) %>% 
+    mutate(
+      sallev = pmap(list(data, levs), function(data, levs){
+        # browser()
+        out <- data %>% 
+          mutate(
+            saval = salev,
+            salev = cut(salev, breaks = c(-Inf, levs$qts, Inf), labels = lbs),
+            salev = as.character(salev)
+          )
+        
+        return(out)
+        
+      })
+    ) %>% 
+    dplyr::select(-data, -levs) %>% 
+    unnest
+  
+  return(allchg)
+  
+}
