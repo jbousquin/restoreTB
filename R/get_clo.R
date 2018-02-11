3d282411bf6f8cc2fe3f7d227232e4c79ff2eddf
fawda123 <mbafs2012@gmail.com>
2018-02-11 09:52:47 -0800

    distance matching function in km

--- R/get_clo.R
+++ R/get_clo.R
@@ -17,6 +17,32 @@ get_clo <- function(restdat, reststat, wqstat, resgrp = 'top', mtch = 10){
   if(mtch > minval)
     stop(paste('maximum value for mtch is', minval))
 
+  # restall to utm
+  restall <- restall %>% 
+    st_as_sf(coords = c('lon', 'lat')) %>% 
+    st_set_crs('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs') %>% 
+    as('Spatial') %>% 
+    spTransform(CRS("+proj=utm +zone=17. +datum=WGS84")) %>% 
+    data.frame %>% 
+    select(-optional) %>% 
+    rename(
+      lon = coords.x1, 
+      lat = coords.x2
+    )
+    
+  # wqstat to utm
+  wqstat <- wqstat %>% 
+    st_as_sf(coords = c('lon', 'lat')) %>% 
+    st_set_crs('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs') %>% 
+    as('Spatial') %>% 
+    spTransform(CRS("+proj=utm +zone=17. +datum=WGS84")) %>% 
+    data.frame %>% 
+    select(-optional) %>% 
+    rename(
+      lon = coords.x1,
+      lat = coords.x2
+    )
+
   # match habitat restoration locations with wq stations by closest mtch locations
   wqmtch <- wqstat %>% 
     group_by(stat) %>% 
@@ -26,7 +52,13 @@ get_clo <- function(restdat, reststat, wqstat, resgrp = 'top', mtch = 10){
         
         # get top mtch closest restoration projects to each station
         # grouped by resgrp column
-        dists <- distm(rbind(sta, restall[, c('lat', 'lon')])) %>%
+        dists <- rbind(sta, restall[, c('lon', 'lat')]) %>%
+          mutate(
+            lon = lon,
+            lat = lat
+          ) %>% 
+          dist(method = 'euclidean') %>% 
+          as.matrix %>% 
           .[-1, 1] %>%
           data.frame(
             restall[, c('id', 'resgrp')],
@@ -39,7 +71,8 @@ get_clo <- function(restdat, reststat, wqstat, resgrp = 'top', mtch = 10){
             data = map(data, function(x) x[1:mtch, ]),
             rnk = map(data, function(x) seq(1:nrow(x)))
           ) %>% 
-          unnest
+          unnest %>% 
+          mutate(dist = dist / 1000) # to km
         
         return(dists)
         
